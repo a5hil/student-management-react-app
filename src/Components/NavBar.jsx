701a71e6f7d57b8a93134414a13953608d128fb8
a5hil <ashil555anto@gmail.com>
2026-07-10 09:09:41 +0530

    SPA Link added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Student Management</a>
+                    <Link className="navbar-brand" to="/">Student Management</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-link" href="/">Add Student</a>
-                            <a className="nav-link" href="/search">Search Student</a>
-                            <a className="nav-link" href="/delete">Delete Student</a>
-                            <a className="nav-link" href="/view">View Students</a>
-
+                            <Link className="nav-link" to="/">Add Student</Link>
+                            <Link className="nav-link" to="/search">Search Student</Link>
+                            <Link className="nav-link" to="/delete">Delete Student</Link>
+                            <Link className="nav-link" to="/view">View Students</Link>
                         </div>
                     </div>
                 </div>
